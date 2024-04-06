059ad8115b5c1a45778db6b14c3b4da06f7c7c2d
[CCM 201 PC] Angus Goucher <gus.goucher@gmail.com>
2024-04-06 18:21:25 -0400

    adding links / attaching

--- src/sm/shadermaker.cpp
+++ src/sm/shadermaker.cpp
@@ -1,6 +1,9 @@
 #include "shadermaker.h"
 
+#include <imgui.h>
 #include <imgui_node_editor.h>
+
+#define ICON_SIZE 16
 
 using namespace sm::maker;
 
@@ -8,11 +11,44 @@ Window::Window()
 {
 	ax::NodeEditor::Config nodesConfig;
 	context = ax::NodeEditor::CreateEditor(&nodesConfig);
+
+	nodes.emplace_back(nextId++, "Smoothie");
+	nodes.back().inputs.emplace_back(nextId++, "Strawberry", PinType::FLOAT);
+	nodes.back().inputs.emplace_back(nextId++, "Kiwi", PinType::FLOAT);
+	int out = nextId++;
+	nodes.back().outputs.emplace_back(out, "Banana", PinType::INT);
+	buildNode(&nodes.back());
+
+	nodes.emplace_back(nextId++, "Smoothie");
+	nodes.back().inputs.emplace_back(nextId++, "Strawberry", PinType::FLOAT);
+	int in = nextId++;
+	nodes.back().inputs.emplace_back(in, "Kiwi", PinType::FLOAT);
+	nodes.back().outputs.emplace_back(nextId++, "Banana", PinType::INT);
+	buildNode(&nodes.back());
+
+	links.emplace_back(nextId++, out, in);
+
+	iconsTexture = new Texture("assets/textures/icons/icons.png");
 }
 
-sm::maker::Window::~Window()
+Window::~Window()
 {
 	ax::NodeEditor::DestroyEditor(context);
+}
+
+void Window::buildNode(Node* node)
+{
+	for (auto& input : node->inputs)
+	{
+		input.node = node;
+		input.kind = ax::NodeEditor::PinKind::Input;
+	}
+
+	for (auto& output : node->outputs)
+	{
+		output.node = node;
+		output.kind = ax::NodeEditor::PinKind::Output;
+	}
 }
 
 void Window::doGui()
@@ -21,25 +57,77 @@ void Window::doGui()
 	ax::NodeEditor::Begin("Nodes");
 	{
 		int uniqueId = 1;
-		ax::NodeEditor::BeginNode(uniqueId++);
-		{
-			ImGui::Text("Node A");
 
-			ax::NodeEditor::BeginPin(uniqueId++, ax::NodeEditor::PinKind::Input);
+		for (Node node : nodes) {
+			ax::NodeEditor::BeginNode(uniqueId++);
 			{
-				ImGui::Text("In");
-			}
-			ax::NodeEditor::EndPin();
+				ImGui::Text(node.name.c_str());
 
-			ImGui::SameLine();
+				for (Link link : links) {
+					ax::NodeEditor::Link(link.id, link.startPinId, link.endPinId);
+				}
 
-			ax::NodeEditor::BeginPin(uniqueId++, ax::NodeEditor::PinKind::Output);
-			{
-				ImGui::Text("Out");
+				ImGui::BeginGroup();
+				for (Pin inputPin : node.inputs) {
+					ax::NodeEditor::BeginPin(uniqueId++, inputPin.kind);
+					{
+						// Icon
+						ImVec2 uv0, uv1;
+						this->getPinTypeTexCoords(inputPin.type, &uv0, &uv1);
+						ImGui::Image((ImTextureID)iconsTexture->getId(), ImVec2{ ICON_SIZE, ICON_SIZE }, uv0, uv1);
+
+						ImGui::SameLine();
+						
+						ImGui::Text(inputPin.name.c_str());
+					}
+					ax::NodeEditor::EndPin();
+				}
+				ImGui::EndGroup();
+				
+				ImGui::SameLine();
+
+				ImGui::BeginGroup();
+				for (Pin outputPin : node.outputs) {
+					ax::NodeEditor::BeginPin(uniqueId++, outputPin.kind);
+					{
+						ImGui::Text(outputPin.name.c_str());
+						
+						ImGui::SameLine();
+
+						// Icon
+						ImVec2 uv0, uv1;
+						this->getPinTypeTexCoords(outputPin.type, &uv0, &uv1);
+						ImGui::Image((ImTextureID)iconsTexture->getId(), ImVec2{ ICON_SIZE, ICON_SIZE }, uv0, uv1);
+					}
+					ax::NodeEditor::EndPin();
+				}
+				ImGui::EndGroup();
 			}
-			ax::NodeEditor::EndPin();
+			ax::NodeEditor::EndNode();
 		}
-		ax::NodeEditor::EndNode();
 	}
 	ax::NodeEditor::End();
+}
+
+void Window::getPinTypeTexCoords(PinType type, ImVec2* uv0, ImVec2* uv1)
+{
+	switch (type)
+	{
+	case sm::maker::PinType::INT:
+	{
+		*uv0 = ImVec2{ 0.5f, 0.f };
+		*uv1 = ImVec2{ 1.f, 0.5f };
+		break;
+	}
+	case sm::maker::PinType::FLOAT:
+	case sm::maker::PinType::VEC2:
+	case sm::maker::PinType::VEC3:
+	case sm::maker::PinType::VEC4:
+	default:
+	{
+		*uv0 = ImVec2{ 0.f, 0.f };
+		*uv1 = ImVec2{ 0.5f, 0.5f };
+		break;
+	}
+	}
 }
--- src/sm/shadermaker.h
+++ src/sm/shadermaker.h
@@ -3,6 +3,7 @@
 #include <imgui_node_editor.h>
 #include <vector>
 #include <string>
+#include "texture.h"
 
 namespace sm {
 	namespace maker {
@@ -18,15 +19,18 @@ namespace sm {
 
 		struct Pin {
 			ax::NodeEditor::PinId id;
+			ax::NodeEditor::PinKind kind;
+
 			Node* node;
 			std::string name;
 			PinType type;
 
-			Pin(int32_t id, const std::string& name, PinType type) : id(id), name(name), type(type) {}
+			Pin(int32_t id, const std::string& name, PinType type) : id(id), name(name), type(type), kind(ax::NodeEditor::PinKind::Input), node(nullptr) {}
 		};
 
 		struct Node {
 			ax::NodeEditor::NodeId id;
+
 			std::string name;
 			std::vector<Pin> inputs;
 			std::vector<Pin> outputs;
@@ -34,16 +38,32 @@ namespace sm {
 			Node(int32_t id, const std::string& name) : id(id), name(name) {}
 		};
 
+		struct Link {
+			ax::NodeEditor::LinkId id;
+
+			ax::NodeEditor::PinId startPinId;
+			ax::NodeEditor::PinId endPinId;
+
+			Link(int32_t id, ax::NodeEditor::PinId start, ax::NodeEditor::PinId end) : id(id), startPinId(start), endPinId(end) {}
+		};
+
 		class Window {
 		public:
 			Window();
 			~Window();
 
+			void buildNode(Node* node);
 			void doGui();
+			void getPinTypeTexCoords(PinType type, ImVec2* uv0, ImVec2* uv1);
 
 		private:
+			uint32_t nextId;
+
 			ax::NodeEditor::EditorContext* context;
+			sm::Texture* iconsTexture;
+
 			std::vector<Node> nodes;
+			std::vector<Link> links;
 		};
 	}
 }
